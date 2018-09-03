ad87c433de608ed31135858b030d024833bc0a74
Koike Kazuhiko <kazhik@gmail.com>
2018-09-03 21:29:39 +0900

    remove ngrok.disconnect on exit: it's not necessary

--- example.js
+++ example.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var googlehome = require('./google-home-notifier');
 var ngrok = require('ngrok');
-var exitHook = require('exit-hook');
 var bodyParser = require('body-parser');
 var app = express();
 const serverPort = 8091; // default port
@@ -133,6 +132,3 @@ app.listen(serverPort, function () {
   });
 });
 
-exitHook(() => {
-  ngrok.disconnect();
-});
