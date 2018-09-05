2bab8b3ff3c5807b6780d0211b06985fe2335bbc
Koike Kazuhiko <kazhik@gmail.com>
2018-09-05 17:10:45 +0900

    added tests

--- test/testlib.js
+++ test/testlib.js
@@ -8,8 +8,13 @@ var deviceAddress = '192.168.1.4';
 describe('google-home-notifier library test', function() {
   this.timeout(10000);
 
-  it('device name should be supplied before notify/play', function(done) {
-    googlehome.notify('notify no.1', (notifyRes) => {
+  beforeEach(function() {
+    googlehome.ip(undefined)
+      .device(undefined);
+  });
+
+  it('No device name and address', function(done) {
+    googlehome.notify('no device name and address', (notifyRes) => {
       expect(notifyRes).to.be(undefined);
       setTimeout(done, 1000);
     });
@@ -39,34 +44,90 @@ describe('google-home-notifier library test', function() {
 
   it('ip address is supplied', function(done) {
     googlehome.ip(deviceAddress)
-      .language('ja')
+      .language('en')
       .notify('ip address is supplied', (notifyRes) => {
+        expect(notifyRes).not.to.be(undefined);
+        setTimeout(done, 3000);
+      });
+  });
+
+  it('ip address and invalid device name', function(done) {
+    googlehome.ip(deviceAddress)
+      .device('Invalid name')
+      .notify('ip address and invalid device name', (notifyRes) => {
         expect(notifyRes).not.to.be(undefined);
         setTimeout(done, 3000);
       });
   });
  
   it('invalid timeout value should be ignored', function(done) {
-    googlehome.timeout('aaa')
+    googlehome.ip(deviceAddress)
+      .timeout('aaa')
       .notify('invalid timeout value', (notifyRes) => {
         expect(notifyRes).not.to.be(undefined);
         setTimeout(done, 3000);
       });
   });
 
-  if('invalid speed value should be ignored', function(done) {
-    googlehome.speed('bbb')
+  it('invalid speed value should be ignored', function(done) {
+    googlehome.ip(deviceAddress)
+      .speed('bbb')
       .notify('invalid speed value', (notifyRes) => {
         expect(notifyRes).not.to.be(undefined);
         setTimeout(done, 3000);
       });
   });
 
-  if('invalid volume value should be ignored', function(done) {
-    googlehome.volume('ccc')
+  it('invalid volume value should be ignored', function(done) {
+    googlehome.ip(deviceAddress)
+      .volume('ccc')
       .notify('invalid volume value', (notifyRes) => {
         expect(notifyRes).not.to.be(undefined);
         setTimeout(done, 3000);
+      });
+  });
+
+  it('invalid ip address', function(done) {
+    googlehome.ip('192.168.1.41')
+      .notify('invalid ip address', (notifyRes) => {
+        expect(notifyRes).to.be(undefined);
+        setTimeout(done, 1000);
+      });
+  });
+
+  /* node-casttv2-client will time out in 2 minutes.
+  it('invalid ip address #2', function(done) {
+    this.timeout(180 * 1000);
+    googlehome.ip('192.168.200.1')
+      .notify('invalid ip address #2', (notifyRes) => {
+        expect(notifyRes).to.be(undefined);
+        setTimeout(done, 1000);
+      });
+  });
+  */
+
+  it('play sound file', function(done) {
+    this.timeout(20 * 1000);
+    googlehome.ip(deviceAddress)
+      .play('https://www.gnu.org/music/FreeSWSong.ogg', (notifyRes) => {
+        expect(notifyRes).not.to.be(undefined);
+        setTimeout(done, 10000);
+      });
+  });
+    
+  it('sound file not found', function(done) {
+    googlehome.ip(deviceAddress)
+      .play('https://www.gnu.org/music/xxxx.ogg', (notifyRes) => {
+        expect(notifyRes).to.be(undefined);
+        setTimeout(done, 1000);
+      });
+  });
+
+  it('unknown sound file format', function(done) {
+    googlehome.ip(deviceAddress)
+      .play('https://www.gnu.org/music/free-software-song.au', (notifyRes) => {
+        expect(notifyRes).to.be(undefined);
+        setTimeout(done, 1000);
       });
   });
     
