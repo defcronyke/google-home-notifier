56d98c78c59784cd57f4f890bfc8eceef54af9f3
Koike Kazuhiko <kazhik@gmail.com>
2018-09-06 00:47:42 +0900

    test coverage

--- test/testlib.js
+++ test/testlib.js
@@ -16,9 +16,10 @@ describe('google-home-notifier library test', function() {
   it('No device name and address', function(done) {
     googlehome.notify('no device name and address', (notifyRes) => {
       expect(notifyRes).to.be(undefined);
-      setTimeout(done, 1000);
+      done();
     });
   });
+
   it('Invalid device name', function(done) {
 
     googlehome.device('InvalidName')
@@ -27,15 +28,26 @@ describe('google-home-notifier library test', function() {
       setTimeout(() => {
         googlehome.notify('Second search of Google Home', (notifyRes) => {
           expect(notifyRes).to.be(undefined);
-          setTimeout(done, 1000);
+          done();
         });
-      }, 1000);
+      }, 0);
     });
+  });
+
+  it('Invalid language', function(done) {
+    googlehome.device(deviceName)
+      .language('xx')
+      .notify('invalid language', (notifyRes) => {
+        expect(notifyRes).to.be(undefined);
+        done();
+      });
   });
 
   it('device name is supplied', function(done) {
     googlehome.device(deviceName)
       .language('ja')
+      .volume(1.0)
+      .speed(1.0)
       .notify('device name is supplied', (notifyRes) => {
         expect(notifyRes).not.to.be(undefined);
         setTimeout(done, 3000);
@@ -45,6 +57,9 @@ describe('google-home-notifier library test', function() {
   it('ip address is supplied', function(done) {
     googlehome.ip(deviceAddress)
       .language('en')
+      .volume(1.0)
+      .speed(1.0)
+      .timeout(1000)
       .notify('ip address is supplied', (notifyRes) => {
         expect(notifyRes).not.to.be(undefined);
         setTimeout(done, 3000);
@@ -54,9 +69,22 @@ describe('google-home-notifier library test', function() {
   it('ip address and invalid device name', function(done) {
     googlehome.ip(deviceAddress)
       .device('Invalid name')
+      .speed(0.5)
+      .timeout(1000)
       .notify('ip address and invalid device name', (notifyRes) => {
         expect(notifyRes).not.to.be(undefined);
         setTimeout(done, 3000);
+      });
+  });
+
+  it('tts timeout', function(done) {
+    googlehome.ip(deviceAddress)
+      .timeout(10)
+      .notify('tts timeout', (notifyRes) => {
+        expect(notifyRes).to.be(undefined);
+
+        googlehome.timeout(1000);
+        done();
       });
   });
  
@@ -87,11 +115,20 @@ describe('google-home-notifier library test', function() {
       });
   });
 
+  it('invalid volume value should be ignored #2', function(done) {
+    googlehome.ip(deviceAddress)
+      .volume(1.1)
+      .notify('invalid volume value #2', (notifyRes) => {
+        expect(notifyRes).not.to.be(undefined);
+        setTimeout(done, 3000);
+      });
+  });
+
   it('invalid ip address', function(done) {
     googlehome.ip('192.168.1.41')
       .notify('invalid ip address', (notifyRes) => {
         expect(notifyRes).to.be(undefined);
-        setTimeout(done, 1000);
+        done();
       });
   });
 
@@ -101,25 +138,40 @@ describe('google-home-notifier library test', function() {
     googlehome.ip('192.168.200.1')
       .notify('invalid ip address #2', (notifyRes) => {
         expect(notifyRes).to.be(undefined);
-        setTimeout(done, 1000);
+        done();
       });
   });
   */
+
+  it('ip address or device name should be supplied before play', function(done) {
+    googlehome.play('https://www.gnu.org/music/FreeSWSong.ogg', (notifyRes) => {
+        expect(notifyRes).to.be(undefined);
+        done();
+      });
+  });
 
   it('play sound file', function(done) {
     this.timeout(20 * 1000);
     googlehome.ip(deviceAddress)
       .play('https://www.gnu.org/music/FreeSWSong.ogg', (notifyRes) => {
         expect(notifyRes).not.to.be(undefined);
-        setTimeout(done, 10000);
+        setTimeout(done, 5000);
       });
   });
     
+  it('play sound file #2', function(done) {
+    this.timeout(20 * 1000);
+    googlehome.device(deviceName)
+      .play('https://www.gnu.org/music/FreeSWSong.ogg', (notifyRes) => {
+        expect(notifyRes).not.to.be(undefined);
+        setTimeout(done, 5000);
+      });
+  });
   it('sound file not found', function(done) {
     googlehome.ip(deviceAddress)
       .play('https://www.gnu.org/music/xxxx.ogg', (notifyRes) => {
         expect(notifyRes).to.be(undefined);
-        setTimeout(done, 1000);
+        done();
       });
   });
 
@@ -127,7 +179,7 @@ describe('google-home-notifier library test', function() {
     googlehome.ip(deviceAddress)
       .play('https://www.gnu.org/music/free-software-song.au', (notifyRes) => {
         expect(notifyRes).to.be(undefined);
-        setTimeout(done, 1000);
+        done();
       });
   });
     
